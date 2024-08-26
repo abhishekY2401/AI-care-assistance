969af9b045a18bfb50c22efa10598effa002caeb
abhishekY2401 <abhiyd2401@gmail.com>
2024-08-26 16:22:33 +0530

    change in query route

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -5,5 +5,5 @@ agent_blueprint = Blueprint('agent_blueprint', __name__)
 
 def add_resources(api):
     """ INIT API ROUTES """
-    api.add_resource(QueryAgentAPI, '/query')
+    api.add_resource(QueryAgentAPI, '/query_llm')
     api.add_resource(HealthAPI, '/health_check')
